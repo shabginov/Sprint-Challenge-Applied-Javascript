467c686a9c4384c44b79f898b097421bdc652fc0
Alexey Shabginov <a.shabginov@gmail.com>
2020-05-17 19:03:12 -0700

    Card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,44 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+  // const articles = response.data.articles;
+  // const keys = Object.keys(articles);
+  // console.log(keys);
+  // console.log(articles);
+  // keys.forEach(article => {
+  //   // console.log(articles[article]);
+  //   entry.appendChild(cardComponent(article));
+  // })
+  response.data.articles.bootstrap.forEach( article => {
+    entry.append(cardComponent(article));
+  })
+})
+.catch(error => error);
+
+function cardComponent(obj) {
+  const card = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const img = document.createElement('img');
+  const authorName = document.createElement('span');
+
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imgContainer.classList.add('img-container');
+  img.setAttribute('href', `${obj.authorPhoto}`);
+  authorName.textContent = `By ${obj.authorName}`;
+
+  card.append(headline, author);
+  author.append(imgContainer, authorName);
+  imgContainer.append(img);
+
+  return card;
+}
+
+const entry = document.querySelector('.cards-container');
